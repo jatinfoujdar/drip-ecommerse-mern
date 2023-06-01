768dff75a3c5abe56b471ecc63783695ed7c05b3
Jatin Foujdar <jatin321001@gmail.com>
2023-06-01 14:20:10 +0530

    data & database fetch commit

--- server/Config/database.js
+++ server/Config/database.js
@@ -1,8 +1,23 @@
 import mongoose from "mongoose";
-import app from "../app.js";
-import config from "./index.js";
 
 
+export const Connection = async()=>{
+    const URL = "mongodb+srv://jatinfoujdar:jatinfoujdar1234@e-commerse.1dtnhmu.mongodb.net/?retryWrites=true&w=majority"
+    try {
+        await mongoose.connect(URL,
+            
+            {
+                useNewUrlParser: true,
+                useUnifiedTopology: true,
+                
+            })
+            console.log("Database connected successfully");
+    } catch (error) {
+        console.log("Error while connecting with the database",error.message);
+    }
+}
+
+export default Connection;
 
 
 
--- server/app.js
+++ server/app.js
@@ -1,13 +1,14 @@
-const express = require('express');
+import express from "express";
 const app = express();
 const PORT = process.env.PORT || 4000;
+import Connection from './Config/database.js';
 
 // Middleware
-app.use(express.json());
+// app.use(express.json());
 
 // Routes
 // app.use('/api/auth', require('./routes/auth'));
-
+Connection()
 // Start the server
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
