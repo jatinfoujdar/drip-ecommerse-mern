4929ec8532fcdb16214266475a8e45614c775734
Jatin Foujdar <jatin321001@gmail.com>
2023-06-12 00:31:18 +0530

    fetch api commit

---
+++ client/src/Components/Products/ProductApi.jsx
@@ -0,0 +1,10 @@
+const fetchProducts = () => {
+    return new Promise((resolve, reject) => {
+      const url = 'https://dummyjson.com/products';
+  
+      fetch(url)
+        .then(response => response.json())
+        .then(data => resolve(data))
+        .catch(error => reject(error));
+    });
+  };
