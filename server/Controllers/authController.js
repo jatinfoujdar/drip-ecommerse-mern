bbad79b5e107d0f983bd987696d78d5544d280e0
Jatin Foujdar <jatin321001@gmail.com>
2023-06-14 18:04:25 +0530

    config middleware app commit

--- server/Controllers/authController.js
+++ server/Controllers/authController.js
@@ -12,28 +12,34 @@ const signupUser = async (req, res) => {
 };
 
 // Login user
+
 const loginUser = async (req, res) => {
   try {
     const { email, password } = req.body;
+
     // Check if user exists
     const user = await User.findOne({ email }).select("+password");
     if (!user) {
       return res.status(404).json({ error: "User not found" });
     }
+
     // Compare entered password with the hashed password in the database
     const isMatch = await user.comparePassword(password);
     if (!isMatch) {
       return res.status(401).json({ error: "Invalid password" });
     }
+
     // Generate JWT token
-    const token = user.generateJwtToken();
+    const token = user.getJwtToken();
     res.status(200).json({ token });
   } catch (error) {
+    console.error(error);
     res.status(500).json({ error: "Failed to login user" });
   }
 };
 
 
+//logout
  const logoutUser = async (req, res) => {
   try {
  
--- server/app.js
+++ server/app.js
@@ -1,12 +1,18 @@
 import express from "express"
-const app = express();
 const PORT = process.env.PORT || 4000;
 import authRoutes from "./Routes/authRoutes.js";
 import Connection from "./Config/database.js";
+import cookieParser from "cookie-parser";
 
-
+const app = express();
 // Middleware
 app.use(express.json());
+app.use(express.urlencoded({extended:true}))
+app.use(cors())
+app.use(cookieParser())
+
+//morgan logger
+app.use(morgan("tiny"))
 
 // Routes
 app.use('/api/v1', authRoutes);
