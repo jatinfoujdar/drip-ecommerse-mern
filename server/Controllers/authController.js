f86bb3841eba0f64ba165615ae3aaffcfb242d96
Jatin Foujdar <jatin321001@gmail.com>
2023-06-15 00:25:29 +0530

    login controller commit

--- server/Controllers/authController.js
+++ server/Controllers/authController.js
@@ -47,47 +47,40 @@ const signupUser = asyncHandler(async (req, res) => {
   });
 });
 
-export default signupUser;
+/*
+ @loginUser
+ route := http://localhost:4000/api/v1/login
+ params:=  email, password
+ details:= new user
+*/
+const loginUser = asyncHandler(async (req, res) => {
+  const { email, password } = req.body;
 
-
-
-// Register a new user
-// const signupUser = async (req, res) => {
-//   try {
-//     const { name, email, password } = req.body;
-//     const user = await User.create({ name, email, password });
-//     res.status(201).json({ user });
-//   } catch (error) {
-//     res.status(500).json({ error: "Failed to register user" });
-//   }
-// };
-
-// Login user
-
-const loginUser = async (req, res) => {
-  try {
-    const { email, password } = req.body;
-
-    // Check if user exists
-    const user = await User.findOne({ email }).select("+password");
-    if (!user) {
-      return res.status(404).json({ error: "User not found" });
-    }
-
-    // Compare entered password with the hashed password in the database
-    const isMatch = await user.comparePassword(password);
-    if (!isMatch) {
-      return res.status(401).json({ error: "Invalid password" });
-    }
-
-    // Generate JWT token
-    const token = user.getJwtToken();
-    res.status(200).json({ token });
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ error: "Failed to login user" });
+  if (!email || !password) {
+    throw new CustomError('Please provide email and password', 400);
   }
-};
+
+  const user = await User.findOne({ email }).select('+password');
+
+  if (!user || !(await user.comparePassword(password))) {
+    throw new CustomError('Invalid email or password', 401);
+  }
+
+  const token = user.getJwtToken();
+  user.password = undefined;
+
+  res.status(200).json({
+    success: true,
+    message: 'User logged in successfully',
+    data: {
+      user,
+      token,
+    },
+  });
+});
+
+
+
 
 
 //logout
